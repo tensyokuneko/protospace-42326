232df8b9e9d8293571a41b8f670b63e6ab4d05b9
tensyokuneko <npon.fafa@gmail.com>
2025-07-26 20:49:25 +0900

    初回コミット

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,80 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  before_action :set_prototype, only: [:edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end  
+
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+    redirect_to root_path
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path and return
+    end
+
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path and return
+    end
+
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_prototype
+  @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @prototype.user
+    redirect_to root_path
+    end
+  end
+
+  def prototype_params
+    params.require(:prototype)
+    .permit(:title, :catch_copy, :concept, :image)
+    .merge(user_id: current_user.id)
+  end
+  
+end
+
